76fa2180c5c48a574bc0e031942230a29d10aef4
admin <v@vvzl.ru>
2018-06-18 15:23:36 +0300

    update 2

--- src/unit1/App.java
+++ src/unit1/App.java
@@ -1,5 +1,7 @@
 package unit1;
 
+import jdk.nashorn.internal.objects.annotations.Constructor;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -7,7 +9,7 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class App {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException {
 
         ArrayList<Person> persons = new ArrayList<>();
 
@@ -25,15 +27,16 @@ public class App {
                 Person person =new Person();
 
                 Class classPerson = person.getClass();
+                Person person1 = (Person) classPerson.newInstance();
 
                 Field fields[] = classPerson.getDeclaredFields();
 
                 for (int i = 0; i <fields.length ; i++) {
                     fields[i].setAccessible(true);
-                    fields[i].set(person,strings[i]);
+                    fields[i].set(person1,strings[i]);
                 }
 
-                persons.add(person);
+                persons.add(person1);
 
             }
 
